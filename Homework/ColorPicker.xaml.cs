1561667812da701f3892195e5b390a5c4ae1b5bc
Ольга Черепанова <chernichka.ekb@gmail.com>
2022-01-13 10:15:09 +0500

    Добавьте файлы проекта.

---
+++ Homework/ColorPicker.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        public static DependencyProperty ColorProperty;
+        public static DependencyProperty RedProperty;
+        public static DependencyProperty GreenProperty;
+        public static DependencyProperty BlueProperty;
+
+        static ColorPicker()
+        {
+            ColorProperty = DependencyProperty.Register(
+                nameof(Color),
+                typeof(Color),
+                typeof(ColorPicker),
+                new FrameworkPropertyMetadata(
+                    Colors.Black,
+                    new PropertyChangedCallback(OnColorChanged)));
+            RedProperty = DependencyProperty.Register(
+                            nameof(Red),
+                            typeof(Byte),
+                            typeof(ColorPicker),
+                            new FrameworkPropertyMetadata(
+                              new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register(
+                            nameof(Green),
+                            typeof(Byte),
+                            typeof(ColorPicker),
+                            new FrameworkPropertyMetadata(
+                              new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register(
+                            nameof(Blue),
+                            typeof(Byte),
+                            typeof(ColorPicker),
+                            new FrameworkPropertyMetadata(
+                              new PropertyChangedCallback(OnColorRGBChanged)));
+        }
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor=(Color)e.NewValue;
+            ColorPicker colorPicker = (ColorPicker)sender;
+            colorPicker.Red = newColor.R;
+            colorPicker.Green = newColor.G;
+            colorPicker.Blue = newColor.B;
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorPicker colorPicker=(ColorPicker)sender;
+            Color color = colorPicker.Color;
+            if (e.Property==RedProperty)
+            {
+                color.R = (byte)e.NewValue;
+            }
+            if (e.Property == GreenProperty)
+            {
+                color.G = (byte)e.NewValue;
+            }
+            if (e.Property == BlueProperty)
+            {
+                color.B = (byte)e.NewValue;
+            }
+            colorPicker.Color = color;
+        }
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public Byte Red
+        {
+            get { return (Byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public Byte Green
+        {
+            get { return (Byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public Byte Blue
+        {
+            get { return (Byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+        public static readonly RoutedEvent ColorChangedEvent=
+                     EventManager.RegisterRoutedEvent(
+                         nameof(ColorChanged),
+                         RoutingStrategy.Bubble,
+                         typeof(RoutedPropertyChangedEventHandler<Color>), 
+                         typeof(ColorPicker));
+        public event RoutedPropertyChangedEventHandler<Color>ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent, value); }
+        }
+            public ColorPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
